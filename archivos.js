921fa17e67d1be0feb15ae822e5e8db22c592c19
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2026-05-26 07:48:16 -0400

    update

---
+++ archivos.js
@@ -0,0 +1,7 @@
+// Lista de archivos CSV dentro de la carpeta /doc
+const misArchivosCSV = [
+    "VerbosIrregulares.csv",
+    "vocabulario_tecnico.csv",
+    "expresiones_comunes.csv"
+    // Agrega aquí los nombres exactos de los demás archivos que tengas
+];
